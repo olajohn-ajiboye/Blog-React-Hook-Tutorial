8daaa5d9c177d05555b83a1f7fcf04a43ca1dfab
mOnGoParK <johncode10.17@gmail.com>
2019-06-27 00:27:11 +0300

    Created Hook and Effect in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [url] = useState(`https://api.myjson.com/bins/t7szj`)
+  const [recipes, setRecipes] = useState([])
+  const fetchRecipe = async () => {
+    const recipeData = await fetch(url)
+    const { recipes } = await recipeData.json()
+    setRecipes(recipes)
+  }
+  useEffect(() => {
+    fetchRecipe()
+  })
   return (
     <div className="App">
-      Hello There
+      {/* Use response somehow */}
     </div>
   );
 }
