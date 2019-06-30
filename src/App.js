48de215e2f8f1069317d09825dd4f689bbd8e2b9
mOnGoParK <johncode10.17@gmail.com>
2019-06-30 20:50:09 +0300

    Added home re-route

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import RecipeList from './components/RecipeList'
 
 
 function App() {
-  const apiKey = `36920f6651c9cd9d91a6c3205cabaa19`
-  const url = `https://www.food2fork.com/api/search?key=${apiKey}`
+  const apiKey = `36920f6651c9dcd9d91a6c3205cabaa19`
+  let url = `https://www.food2fork.com/ddapi/search?key=${apiKey}`
   const [showHomeButton, setShowHomeButton] = useState(false)
   const [recipes, setRecipes] = useState([])
   const [loading, setLoading] = useState(true)
@@ -12,22 +12,33 @@ function App() {
 
 
   const fetchRecipe = async () => {
-    const recipeData = await fetch(url)
-    const { recipes } = await recipeData.json()
-    setRecipes(recipes)
-    setLoading(false)
-
+    try {
+      const recipeData = await fetch(url)
+      const { recipes } = await recipeData.json()
+      setRecipes(recipes)
+      setLoading(false)
+    } catch (e) {
+      if (e) {
+        const recipeData = await fetch(`https://api.myjson.com/bins/t7szj`)
+        const { recipes } = await recipeData.json()
+        setRecipes(recipes)
+        setLoading(false)
+      }
+    }
   }
   const handleSubmit = async (e) => {
     e.preventDefault()
-    setLoading(true)
-    const searchUrl = `${url}&q=${search}`
-    const searchedRecipeData = await fetch(searchUrl)
-    const { recipes } = await searchedRecipeData.json()
-    setRecipes(recipes)
-    setLoading(false)
-    setShowHomeButton(true)
-
+    try {
+      setLoading(true)
+      const searchUrl = `${url}&q=${search}`
+      const searchedRecipeData = await fetch(searchUrl)
+      const { recipes } = await searchedRecipeData.json()
+      setRecipes(recipes)
+      setLoading(false)
+      setShowHomeButton(true)
+    } catch (e) {
+      console.log(e)
+    }
   }
   const handleSearchChange = (e) => {
     setSearch(e.target.value)
